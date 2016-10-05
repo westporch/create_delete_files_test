8d430c654d8775e7e50315c71ecc044f8ffe3bc1
Westporch <westporch@gmail.com>
2016-10-05 21:22:39 +0900

    반복문 수정

--- create_delete_files_test.sh
+++ create_delete_files_test.sh
@@ -2,7 +2,7 @@
 # Hyun-gwan Seo
 
 BASE_DIR=/data
-ITERATION=1
+ITERATION=100
 FILE_SIZE=1M
 
 function get_inode_info()
@@ -11,19 +11,19 @@ function get_inode_info()
 	df -i | grep /data
 }
 
-echo "파일 생성 전 /data(ocfs2 파일 시스템)의 inode 정보"
+echo -e "\t파일 생성 전 /data(ocfs2 파일 시스템)의 inode 정보"
 get_inode_info
 
 for((idx = 0; idx < $ITERATION; idx++))
 do
-	dd if=/dev/zero of=/$BASE_DIR/file_test-$ITERATION.txt bs=$FILE_SIZE count=1
+	dd if=/dev/zero of=/$BASE_DIR/file_test-$idx.txt bs=$FILE_SIZE count=1 &> /dev/null
 done
 
-echo "파일 생성 후 /data(ocfs2 파일 시스템)의 inode 정보"
+echo -e "\t파일 생성 후 /data(ocfs2 파일 시스템)의 inode 정보"
 get_inode_info
 
 sleep 5
 rm -rf $BASE_DIR/*.txt
 
-echo "파일 삭제 후 /data(ocfs2 파일 시스템)의 inode 정보"
+echo -e "\t파일 삭제 후 /data(ocfs2 파일 시스템)의 inode 정보"
 get_inode_info
